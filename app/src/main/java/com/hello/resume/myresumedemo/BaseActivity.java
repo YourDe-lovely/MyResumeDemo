aa9aa705b1f73ade906d0b26a71a079da3480bfd
baohaiben <itbaodaren@163.com>
2019-03-21 21:50:13 +0800

    no message

--- app/src/main/java/com/hello/resume/myresumedemo/BaseActivity.java
+++ app/src/main/java/com/hello/resume/myresumedemo/BaseActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity  {
 
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
